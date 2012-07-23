453be0942a8dffded309b86a13e099f57dc127e4
Endoze <endoze@endozemedia.com>
2012-07-23 16:03:00 -0400

    Initial Commit

---
+++ MenuletExample/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  MenuletExample
+//
+//  Created by Endoze on 7/23/12.
+//  Copyright (c) 2012 EndozeMedia, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@property (nonatomic, retain) NSStatusItem *statusItem;
+@property (nonatomic, retain) IBOutlet NSMenu *menu;
+
+@end
---
+++ MenuletExample/AppController.m
@@ -0,0 +1,32 @@
+//
+//  AppController.m
+//  MenuletExample
+//
+//  Created by Endoze on 7/23/12.
+//  Copyright (c) 2012 EndozeMedia, LLC. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@synthesize statusItem = _statusItem;
+@synthesize menu = _menu;
+
+- (void)awakeFromNib
+{
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [self.statusItem setHighlightMode:YES];
+    [self.statusItem setTitle: @"Menulet Example"];
+    [self.statusItem setEnabled:YES];
+    [self.statusItem setToolTip:@"IPMenulet"];
+    [self.statusItem setMenu:self.menu];
+    
+}
+
+-(IBAction)terminate:(id)sender
+{
+    exit(EXIT_SUCCESS);
+}
+
+@end
